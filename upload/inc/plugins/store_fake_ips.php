19fe20e62fa642bb37debb6315caede4320b4fef
Przemek Pawlas <Destroy666@interia.pl>
2015-12-27 17:50:53 +0100

    Useless variable
    ..

--- upload/inc/plugins/store_fake_ips.php
+++ upload/inc/plugins/store_fake_ips.php
@@ -50,7 +50,7 @@ SFI;
 	return array(
 		'name'			=> $lang->store_fake_ips,
 		'description'	=> $lang->store_fake_ips_info.'<br />
-'.$store_fake_ips_pl.$store_fake_ips_cfg.'
+'.$store_fake_ips_cfg.'
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="ZRC6HPQ46HPVN">
